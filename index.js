7be7287a0b33b3aa6c1b313a91ddbe0938b4e391
7.studio <mail@julianejeske.com>
2026-07-16 11:41:46 +0200

    js query selector setup and variables

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// phew… not a lot going on here. Please add some code!
+ //phew… not a lot going on here. Please add some code!
+
+console.log("JS is running");
+const bookmarkBtn = document.querySelector('[data-js="bookmark-btn"]')
+const answerBtn = document.querySelector('[data-js="answer-btn"]')
+const answer = document.querySelector('[data-js="answer"]');
+
+// CHECK IF THE ELEMENTS ARE FOUND
+console.log(answerBtn);
+console.log(answer);
+
+// FIRST SOLUTION WITH a new CLASS .hidden
+// function showAnswers() {
+// answer.classList.toggle("hidden");
+// }
+
+// SECOND SOLUTION WITH a new CLASS BIM .card__answer--active
+function showAnswers() {
+  answer.classList.toggle("card__answer--active");
+  console.log("card__answer--active");
+}
+// answerBtn.addEventListener("click", showAnswers);
+
+answerBtn.addEventListener("click", showAnswers);
+
+ function bookmarkQuestion() {
+  // Function body
+}
